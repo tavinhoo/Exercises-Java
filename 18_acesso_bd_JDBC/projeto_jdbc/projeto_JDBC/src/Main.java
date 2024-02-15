50d8ba7d764c81768c7e01717f6f2acacc474880
Otávio <luizotaviomtbs@gmail.com>
2024-02-14 21:44:57 -0300

    Statemente e ResultSet - AULA 270

--- 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/Main.java
+++ 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/Main.java
@@ -1,15 +1,40 @@
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
+        // CÓDIGO DAS AULAS 268 269
+//        /* Sempre que precisar de conexão com banco usar: DB.getConnection(); */
+//        Connection conn = DB.getConnection();
+//
+//        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
-        /* Sempre que precisar de conexão com banco usar: DB.getConnection(); */
-        Connection conn = DB.getConnection();
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM DEPARTMENT");
 
+            /* COMO PERCORRER OS DADOS DO RESULSET? */
+            /* rs.next() enquanto encontra = true, caso não encontre = false */
 
-        DB.closeConnection();
+            while(rs.next()) {
+                System.out.println(rs.getInt("ID") + ", " + rs.getString("NAME"));
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
--- 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/db/DB.java
+++ 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -48,5 +46,27 @@ public class DB {
        } catch(IOException e) {
             throw new DbException(e.getMessage());
        }
+    }
+
+    /* ABAIXO OS MÉTODOS AUXILIARES PARA EVITAR TRY CATCH NO PROGRAMA MAIN*/
+
+    public static void closeStatement(Statement st) {
+       if(st != null) {
+           try {
+               st.close();
+           } catch (SQLException e) {
+               throw new DbException(e.getMessage());
+           }
+       }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if(rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 }
