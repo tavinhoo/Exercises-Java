2d86ea997593b4d007d9eff7f3c8c38a5ee58c96
Otávio <luizotaviomtbs@gmail.com>
2024-02-15 18:52:09 -0300

    preparedStatement -> Inserção de dados a partir do placeholder

--- 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/Main.java
+++ 18_acesso_bd_JDBC/projeto_jdbc/projeto_JDBC/src/Main.java
@@ -1,10 +1,10 @@
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,26 +14,94 @@ public class Main {
 //        Connection conn = DB.getConnection();
 //
 //        DB.closeConnection();
+
+        // ATÉ O BLOCO FINALLY = AULA 270
+     /*
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
+    */
+//        try {
+//            conn = DB.getConnection();
+//            st = conn.createStatement();
+//            rs = st.executeQuery("SELECT * FROM DEPARTMENT");
+//
+//            /* COMO PERCORRER OS DADOS DO RESULSET? */
+//            /* rs.next() enquanto encontra = true, caso não encontre = false */
+//
+//            while(rs.next()) {
+//                System.out.println(rs.getInt("ID") + ", " + rs.getString("NAME"));
+//            }
+//        } catch (SQLException e) {
+//            throw new DbException(e.getMessage());
+//        } finally {
+//            DB.closeResultSet(rs);
+//            DB.closeStatement(st);
+//            DB.closeConnection();
+//        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Connection conn = null;
+        // ASSUNTO DA AULA
+        PreparedStatement pst = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.createStatement();
-            rs = st.executeQuery("SELECT * FROM DEPARTMENT");
+            // O "?" é chamado de PLACEHOLDER
+            // Permite que o valor seja inserido posteriormente
 
-            /* COMO PERCORRER OS DADOS DO RESULSET? */
-            /* rs.next() enquanto encontra = true, caso não encontre = false */
+            // 1ª FORMA -> PreparedStatement and executeUpdate();
+            /*
+            pst = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+                            "VALUES" +
+                            "(?, ?, ?, ?, ?);");
+            pst.setString(1, "Carl Purple");
+            pst.setString(2, "carlpurple@gmail.com");
+            pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            pst.setDouble(4, 3000.0);
+            pst.setInt(5, 4);
+            */
+            // 2ª FORMA -> Statement.RETURN_GENERATED_KEYS; (RECUPERANDO ID INSERIDO)
+            //           -> getGeneratedKeys();
 
-            while(rs.next()) {
-                System.out.println(rs.getInt("ID") + ", " + rs.getString("NAME"));
+//            pst = conn.prepareStatement(
+//                    "INSERT INTO seller "
+//                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+//                            "VALUES" +
+//                            "(?, ?, ?, ?, ?);",
+//                    Statement.RETURN_GENERATED_KEYS);
+//            pst.setString(1, "Carl Purple");
+//            pst.setString(2, "carlpurple@gmail.com");
+//            pst.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+//            pst.setDouble(4, 3000.0);
+//            pst.setInt(5, 4);
+            
+            pst = conn.prepareStatement(
+                    "INSERT INTO department (Name) values ('D1'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            int rowAffecteds = pst.executeUpdate();
+
+            if(rowAffecteds > 0) {
+                ResultSet rs = pst.getGeneratedKeys();
+                while(rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id =" + id);
+                }
+            } else {
+                System.out.println("No rows affected!!");
             }
         } catch (SQLException e) {
-            throw new DbException(e.getMessage());
-        } finally {
-            DB.closeResultSet(rs);
-            DB.closeStatement(st);
+            e.printStackTrace();
+        }
+//        catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+        finally {
+            DB.closeStatement(pst);
             DB.closeConnection();
         }
     }
