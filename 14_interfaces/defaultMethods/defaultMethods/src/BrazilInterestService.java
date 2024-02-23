a2184f1ba25ec5c1280cc9a5b5861ad1ea5326a5
Otávio <luizotaviomtbs@gmail.com>
2024-02-22 22:27:42 -0300

    JDBC - apoio

---
+++ 14_interfaces/defaultMethods/defaultMethods/src/BrazilInterestService.java
@@ -0,0 +1,21 @@
+public class BrazilInterestService extends AbstractInterestService implements InterestService {
+
+    public BrazilInterestService(Double interestRate) {
+        super(interestRate);
+    }
+
+    // private Double interestRate;
+
+    // public BrazilInterestService(Double interestRate) {
+    // this.interestRate = interestRate;
+    // }
+
+    // public double getInterestRate() {
+    // return this.interestRate;
+    // }
+
+    // public void setInterestRate(Double interestRate) {
+    // this.interestRate = interestRate;
+    // }
+
+}
